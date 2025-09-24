4691658064c24499130ce1b8b2454f25cc8f330d
zk020106 <zkai0106@163.com>
2025-09-24 09:22:01 +0800

    style: 统一样式入口

---
+++ packages/core/src/style.ts
@@ -0,0 +1,6 @@
+// 样式入口文件
+import "@arco-design/web-vue/dist/arco.css";
+// 导入组件样式
+import "./components/Bubble/bubble.scss";
+import "./components/MarkdownRender/markdown-render.scss";
+import "./components/Input/input.scss";
